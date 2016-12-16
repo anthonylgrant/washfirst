e92c77c5726ab59c8bf3a44fae93324e604df3f4
Behzad Azampour <behy1368@gmail.com>
2016-12-16 01:08:42 +0000

    configured heroko pg db

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+const dbConfig = require("./db/config");
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: dbConfig,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
