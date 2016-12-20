593a23f7bf5947622e9914bbdc247218793457c8
anthonylgrant <anthonylorenzogrant@gmail.com>
2016-12-20 05:31:54 +0000

    MAJOR COMMIT: setup TEST route, connected server to client and passed data using ajax

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,31 @@ import Sidebar from './components/Sidebar.jsx'
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: ""
+    }
+  }
+
+  componentDidMount() {
+    $.ajax({
+      method: 'GET',
+      url: '/test',
+      dataType: 'JSON',
+      success: function(response){
+        console.log('componentDidMount success:', response)
+        this.setState({
+            test: response.hello
+        })
+      }.bind(this)
+    })
+  }
+
   render() {
     return (
       <div>
+        <h1>{this.state.test}</h1>
         <Navbar />
         <Sidebar />
       </div>
