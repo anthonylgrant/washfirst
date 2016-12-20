69494ff60ddfbdc512ba08093360eda3f4d2478f
cody j <codywj777@gmail.com>
2016-12-20 03:32:33 +0000

    array compare function is written

---
+++ app/helpers/array_compare.js
@@ -0,0 +1,46 @@
+/*
+  This function takes two arguments:
+                  1. An array representing preference tags
+                  2. An array representing an item's tags
+  It returns:
+                  1. A number that items can be ranked upon.
+
+*/
+
+function compareArrays(preferences, itemTags) {
+
+  const numOfPreferences = preferences.length;
+  const numOfItemTags = preferences.length;
+  let overlap = 0;
+
+  if(numOfPreferences === 0) {
+    return 0;
+  }
+
+  preferences.forEach((tag) => {
+    if (itemTags.includes(tag)) {
+      overlap += 1;
+    }
+  });
+
+  const relation = (overlap/numOfPreferences);
+
+  console.log('this is number of matching tags', overlap);
+  console.log('this is the relationship value', relation);
+
+  return relation;
+}
+
+module.exports = compareArrays;
+
+// const prefs1 = ['blue', 'solid', 'normcore', 'slim-fit', 'AA', 'henley'];
+// const prefs2 = ['red', 'checkered', 'goth', 'loose', 'gucci', 'baseball'];
+// const prefs3 = ['blue', 'solid', 'normcore', 'slim-fit', 'AA', 'henley', 'this', 'that', 'theOther'];
+// const prefs4 = ['blue', 'solid', 'normcore', 'loose', 'gucci',];
+//
+// const itemTags = ['blue', 'solid', 'normcore', 'slim-fit', 'AA', 'henley'];
+//
+// compareArrays(prefs1, itemTags);
+// compareArrays(prefs2, itemTags);
+// compareArrays(prefs3, itemTags);
+// compareArrays(prefs4, itemTags);
