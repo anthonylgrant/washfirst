3720597b62fcf39726fdb051f1a9805d53c78ed6
Behzad Azampour <behy1368@gmail.com>
2017-01-10 20:46:06 +0000

    restyled the edit my item form

--- client/src/components/User-Profile-Page/MyItem/EditCard.jsx
+++ client/src/components/User-Profile-Page/MyItem/EditCard.jsx
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+const genders = ['-', 'male', 'female'];
+
+const categories = ['-', 'tops', 'bottoms', 'shoes'];
+
+
+const sizes = {
+  tops: ['-', 1, 2, 3, 4, 5, 6],
+  bottoms: ['-', 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44],
+  shoes: ['-', 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+};
+
 class EditCard extends Component {
 
   render() {
@@ -14,35 +25,60 @@ class EditCard extends Component {
           </figure>
         </div>
 
-        <div className='control'>
-          <input type='text' className='input is-primary'
+        <div className='control new-form'>
+
+          <label className='new-label'>Photo: </label>
+          <input type='file' className='upload-input is-primary new-input'
                  placeholder='Image URL' name='img_url'
-                 value={this.props.img_url} onChange={this.props.handleChange}>
+                 onChange={this.props.handleChange} defaultValue={this.props.img_url} >
           </input>
 
-          <input type='text' className='input is-primary'
-                 placeholder='enter type here' name='type'
-                 value={this.props.type} onChange={this.props.handleChange}>
+
+          <label className='new-label'>Category: </label>
+          <div className="select new-input">
+            <select name="type" onChange={this.props.handleChange} defaultText={this.props.type} >
+            {
+              categories.map((category, index) => {
+                return <option key={index}>{category}</option>
+              })
+            }
+            </select>
+          </div>
+
+          <br/>
+
+          <label className='new-label'>Gender: </label>
+          <div className="select new-input">
+            <select name='gender' onChange={this.props.handleChange} selectedValue={this.props.gender} >
+            {
+              genders.map((gender, index) => {
+                return <option key={index}>{gender}</option>
+              })
+            }
+            </select>
+          </div>
+
+          <br />
+
+          <label className='new-label'>Size: </label>
+          <div className="select new-input">
+            <select name='size' onChange={this.props.handleChange} defaultValue={this.props.size} >
+            {
+              this.props.type && sizes[this.props.type].map((size, index) => {
+                return <option key={index}>{size}</option>
+              })
+            }
+            </select>
+          </div>
+
+          <input type='text' className='input is-primary new-input'
+                 placeholder='Enter Tags' name='tags'
+                 defaultValue={this.props.tags} onChange={this.props.handleChange}>
           </input>
 
-          <input type='text' className='input is-primary'
-                 placeholder='enter gender here' name='gender'
-                 value={this.props.gender} onChange={this.props.handleChange}>
-          </input>
-
-          <input type='text' className='input is-primary'
-                 placeholder='enter new size here' name='size'
-                 value={this.props.size} onChange={this.props.handleChange}>
-          </input>
-
-          <input type='text' className='input is-primary'
-                 placeholder='enter tags' name='tags'
-                 value={this.props.tags} onChange={this.props.handleChange}>
-          </input>
-
-          <input type='text' className='input is-primary'
-                 placeholder='enter description' name='description'
-                 value={this.props.description} onChange={this.props.handleChange}>
+          <input type='text' className='input is-primary new-input'
+                 placeholder='Enter Description' name='description'
+                 defaultValue={this.props.description} onChange={this.props.handleChange}>
           </input>
         </div>
 
