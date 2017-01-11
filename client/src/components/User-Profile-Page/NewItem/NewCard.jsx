24f30b1132064b5e52311329f638456ae041e732
Jungmin Cha <ckwjdals90@gmail.com>
2017-01-11 00:44:36 +0000

    deleted console.log

--- client/src/components/User-Profile-Page/NewItem/NewCard.jsx
+++ client/src/components/User-Profile-Page/NewItem/NewCard.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const genders = ['-', 'Male', 'Memale'];
+const genders = ['-', 'Male', 'Female'];
 
 const categories = ['-', 'Tops', 'Bottoms', 'Shoes'];
 
